e96875254841e2c945b4ae2b69fad69578341154
panchalnik <panchalnik285@gmail.com>
2026-07-14 17:30:00 +0530

    smoke test

---
+++ scripts/smoke_test.py
@@ -0,0 +1,31 @@
+import pymysql
+import os
+import sys
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv('/home/jenkins/.env')
+
+
+def test_database():
+    connection = pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        connect_timeout=5
+    )
+
+    connection.close()
+    print("[PASS] ✅ Database Connectivity Check")
+
+
+if __name__ == "__main__":
+    try:
+        test_database()
+        print("Smoke Test Passed.. ✅")
+    except Exception as e:
+        print("Smoke Test Failed.. ❌")
+        print("Error:", e)
+        sys.exit(1)
